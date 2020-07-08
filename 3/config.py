9207e9f873ce7bdd2b79a81f83b407c392bb7f7b
roppan <61796905+roppan@users.noreply.github.com>
2020-07-08 09:35:07 -0400

    Added ETL files

---
+++ 3/config.py
@@ -0,0 +1,12 @@
+sf_username = 'kwamenaabeka10@gmail.com'
+sf_password = 'Potomac10'
+sf_security_token = 'tjgj209NAR4S2Zc8CFoJHrh9'
+
+remote_db_endpoint = 'dartdoesdata.cprhozcsl0wd.us-east-2.rds.amazonaws.com'
+remote_db_port = '3306'
+remote_db_user = 'admin'
+remote_db_pwd = 'atoabekadoreenmichael20'
+remote_db_name = 'gwsis'
+
+
+
